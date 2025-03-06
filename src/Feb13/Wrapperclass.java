79ba58a323f67880ae64748662d40ed25ec09331
Koushikvs8 <koushikvittalshet@gmail.com>
2025-03-06 14:10:03 +0530

    pareselogic

---
+++ src/Feb13/Wrapperclass.java
@@ -0,0 +1,13 @@
+package Feb13;
+
+public class Wrapperclass {
+
+	public static void main(String[] args) {
+		String str="10";
+		     Integer i= Integer.parseInt(str);
+		     
+		     String str3=String.valueOf(i);
+		     System.out.println(str3);
+	}
+	
+}
